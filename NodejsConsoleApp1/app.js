85052eb320abb24f18cab0590782dadc2745f133
LostProcessor <bilbo763@gmail.com>
2022-07-08 12:11:03 +0200

    'minor'

--- NodejsConsoleApp1/app.js
+++ NodejsConsoleApp1/app.js
@@ -1,2 +1,8 @@
-/* Il faut que ca me fasse une blague , le radom avec math radom * 100 . il faut que mes messages types soit dans un objet. \.
- * alors dje log mon message 
+let jokes = ['bananas are the most serious fruits', 'Did u know u are an abstaction of yourself, that make sense right??', 'Being serious lead to suicide', "Asking quesqtion is extremely dangerous", "Trying to understand is an danger", "Love is the same for everyone never think it can be in  some sense uniqueéd", "Dead or alive"]
+
+
+let joke_to_log = Math.floor(Math.random() * 8)
+
+
+console.log(joke_to_log)
+
